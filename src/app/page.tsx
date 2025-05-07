20f1c44d9c2d899033e618a5bc5b336d82f03acb
NETBEINS <Newspay13@gmail.com>
2025-05-07 20:37:22 +0000

    Please where signup text and bonus replace with the card design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-
 import TopHeader from '@/components/exchange/TopHeader';
-import PromoBanner from '@/components/exchange/PromoBanner';
-import ActionButtons from '@/components/exchange/ActionButtons';
 import FeatureCards from '@/components/exchange/FeatureCards';
 import MarketSection from '@/components/exchange/MarketSection';
 import BottomNavBar from '@/components/exchange/BottomNavBar';
+import CardSection from '@/components/exchange/CardSection';
 
 export default function CryptoExchangePage() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <TopHeader />
       <main className="flex-grow overflow-y-auto pb-16 md:pb-0"> {/* Padding bottom for mobile nav bar */}
-        <PromoBanner />
-        <ActionButtons />
+        <CardSection />
         <FeatureCards />
         <MarketSection />
       </main>
@@ -20,3 +17,4 @@ export default function CryptoExchangePage() {
     </div>
   );
 }
+
