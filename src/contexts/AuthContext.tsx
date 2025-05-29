bf7a2e1edf05e1b66b440276fa2fc86676829c87
reysdaaud <reysdaaud@gmail.com>
2025-05-29 14:44:01 +0500

    Delete src directory

--- src/contexts/AuthContext.tsx
+++
@@ -1,5 +0,0 @@
-// src/contexts/AuthContext.tsx
-'use client';
-
-export { AuthProvider, useAuthContext as useAuth } from '@/lib/firebase';
-export type { FirebaseUser, UserProfile } from '@/lib/firebase'; // Export UserProfile type
